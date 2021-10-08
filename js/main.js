a20604d6937dc65d104363b3b5b8bc0b7d13f9f0
sbelachur <selenebelachur@gmail.com>
2021-10-08 17:35:00 +0200

    clase4 reeentrega

--- js/main.js
+++ js/main.js
@@ -10,20 +10,45 @@ let tapTrimestre = "Tap Dance - Trimestral : 155 euros"
 
 let servicio 
 servicio = prompt ("¿Qué servicios deseas contratar? \n" + "1. " + cantoIndividual + "\n2. " + cantoMes + "\n3. " + cantoTrimestre)
-servico = parseInt (servicio)
+servicio = parseInt (servicio)
 
 
-switch (servicio) {
-    case "1": {
-        alert ("El total es de 10 euros")
-    }; break;
-    case "2": {
-        alert ("El total es de 35 euros")
-    }; break;
-    default: {
-        alert ("El total es de 95 euros")
+function servicioCanto (servicio) {
+    switch (servicio) {
+        case 1: {
+            alert ("El total es de 10 euros");
+            return 10;
+        };
+        case 2: {
+            alert ("El total es de 35 euros")
+            return 35;
+        };
+        default: {
+            alert ("El total es de 95 euros");
+            return 95;
+        }
+    }
+}  
+
+
+function servicioTap (servicio2) {
+    switch (servicio2) {
+        case 1: {
+            alert ("El total es de 15 euros");
+            return 15;
+        };
+        case 2: {
+            alert ("El total es de 55 euros");
+            return 55;
+        };
+        default: {
+            alert ("El total es de 155 euros");
+            return 155;
+        }
     }
 }
+
+let total = servicioCanto (servicio);
 
 let confirmacion = confirm ("¿Desea contratar algún otro servicio'")
 
@@ -33,18 +58,12 @@ if (confirmacion == true){
     servicio2 = prompt ("¿Qué servicios deseas contratar? \n" + "1. " + tapIndividual + "\n2. " + tapMes + "\n3. " + tapTrimestre)
     servico2 = parseInt (servicio2)
 
-    switch (servicio2) {
-        case "1": {
-            alert ("El total es de 15 euros")
-        }; break;
-        case "2": {
-            alert ("El total es de 55 euros")
-        }; break;
-        default: {
-            alert ("El total es de 155 euros")
-        }
-    }
+    total += servicioTap(servicio2);
+    alert("El total de todos los servicios es " + total)
+
+    
 
 } else {
-    alert = "El total es "
+    alert = "El total es" + total
 }
+
