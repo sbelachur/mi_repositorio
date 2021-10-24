1dd8a5ea830b54b457b7ef7bca8c175133a6e954
sbelachur <selenebelachur@gmail.com>
2021-10-24 17:36:54 +0200

    clase8

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 
+
+
+// let div = document.getElementById ("canto");
+
+// console.log (div.innerHTML);
+
+
 /*Servicios*/
 
 class Servicio {
@@ -34,32 +41,42 @@ while (tipo != "ninguno" || tipo != "NINGUNO" || tipo != "Ninguno")
 console.log (arrayServicios);
 
 for (var servicio of arrayServicios) {
-    document.write ("<ul><li><h3>Tipo: " + servicio.nombre + "</h3></li>");
-    document.write ("<li><h3>Duracion: " + servicio.duracion + "</h3></li>");
-    document.write ("<li><h3>Precio: " + servicio.precio + "</h3></li>");
-    document.write ("<li><h3>Precio con Tarifa de Servicio: " + servicio.sumarTarifaServicio () + "</h3></li></ul><br>");
-    console.log (servicio.nombre);
-    console.log (servicio.duracion);
-    console.log (servicio.precio);
-    console.log (servicio.sumarTarifaServicio());
+    let divServicio = document.createElement ("div");
+
+    divServicio.innerHTML = `<h3> Tipo: ${servicio.nombre} </h3>
+                            <h3> Duracion: ${servicio.duracion} </h3>
+                            <h3> Precio: ${servicio.precio} </h3>
+                            <h3> Precio con Tarifa de Servicio: ${servicio.sumarTarifaServicio ()}</h3> `;
+    
+    document.body.appendChild(divServicio)
+
+
+    // document.write ("<li><h3>Duracion: " + servicio.duracion + "</h3></li>");
+    // document.write ("<li><h3>Precio: " + servicio.precio + "</h3></li>");
+    // document.write ("<li><h3>Precio con Tarifa de Servicio: " + servicio.sumarTarifaServicio () + "</h3></li></ul><br>");
+    // console.log (servicio.nombre);
+    // console.log (servicio.duracion);
+    // console.log (servicio.precio);
+    // console.log (servicio.sumarTarifaServicio());
 
 }
 
-var ordenarPrecio = [];
 
-ordenarPrecio = arrayServicios.map (elemento => elemento);
-var ordenarPrecio = arrayServicios;
-ordenarPrecio.sort (function (a, b) {
-    return a.precio - b.precio;
-});
-console.log ("Ordenado por precio");
-console.log (ordenarPrecio);
-document.write ("<h3> Servicios ordenados por precio");
+// var ordenarPrecio = [];
 
-for (var servicio of ordenarPrecio) {
-    document.write ("<ul><li><h3> Nombre: " + servicio.nombre + "</h3></li>");
-    document.write ("<li><h3> Duracion: " + servicio.duracion + "</h3></li>");
-    document.write ("<li><h3> Nombre: " + servicio.precio + "</h3></li></lu>");
+// ordenarPrecio = arrayServicios.map (elemento => elemento);
+// var ordenarPrecio = arrayServicios;
+// ordenarPrecio.sort (function (a, b) {
+//     return a.precio - b.precio;
+// });
+// console.log ("Ordenado por precio");
+// console.log (ordenarPrecio);
+// document.write ("<h3> Servicios ordenados por precio");
 
-}
+// for (var servicio of ordenarPrecio) {
+//     document.write ("<ul><li><h3> Nombre: " + servicio.nombre + "</h3></li>");
+//     document.write ("<li><h3> Duracion: " + servicio.duracion + "</h3></li>");
+//     document.write ("<li><h3> Nombre: " + servicio.precio + "</h3></li></lu>");
+
+// }
 
