44e92337d38bdb135f48f90f8600f269edead89e
sbelachur <selenebelachur@gmail.com>
2021-12-03 19:33:54 +0100

    entrega final

--- js/carrito.js
+++ js/carrito.js
@@ -5,25 +5,29 @@
 // const contenedorServicios = document.getElementByClassName ("contenedorServicios");
 // const contenedorServiciosCarrito = document.getElementById ("contendorServiciosCarrito");
 
+const totalCarrito = document.querySelector('.totalCarrito');
 const contenedorServicios = document.querySelector (".contenedorServicios");
-const contenedorServiciosCarrito = document.querySelector (".contenedorServiciosCarrito");
+const contenedorServiciosCarrito = document.querySelector ("#contenedorServiciosCarrito");
+const vaciar = document.querySelector('#vaciar')
+
 
 comprar ();
 
 function comprar () {
     contenedorServicios.addEventListener("click", reservarServicio);
+    vaciar.addEventListener("click", vaciarCarrito);
     document.addEventListener("DOMContentLoaded", leerLocalStorage);
 }
  
 function reservarServicio (e) {
-
-    if (e.target.classList.contains ("agregarCarrito")) {
-        const servicio = document.querySelector(".servicio");
+    e.preventDefault();
+    if (e.target.classList.contains ('agregarCarrito')) {
+        const servicio = e.target.parentElement;
         datosServicio (servicio);
     }
 }
 
-function datosServicio () {
+function datosServicio (servicio) {
     const infoServicio = {
         titulo: servicio.querySelector('.tituloServicio').textContent,
         precio: servicio.querySelector('.precioServicio').textContent,
@@ -33,47 +37,58 @@ function datosServicio () {
 }
 
 function añadirCarrito (servicio) {
-    const nuevoServicio = document.createElement ('p');
+    const nuevoServicio = document.createElement ('div');
     nuevoServicio.innerHTML = `
 
-    <p> ${titulo.servicio} </p>
-    <p> ${precio.servicio} </p>
+    <p class="estilo"> ${servicio.titulo} = ${servicio.precio}</p>
     `;
 
-    contenedorServiciosCarrito.appendChild (nuevoServicio);
+    contenedorServiciosCarrito.appendChild(nuevoServicio);
     servicioLocalStorage (servicio);
 
+}
+
+function vaciarCarrito(){
+    while(contenedorServiciosCarrito.firstChild){
+        contenedorServiciosCarrito.removeChild(contenedorServiciosCarrito.firstChild);
+    }
+    vaciarLocalStorage ();
+
+    return false;
+}
+
+function vaciarLocalStorage() {
+    localStorage.clear();
 }
 
 function servicioLocalStorage (servicio) {
 
     let servicios;
-    servicios = obtenerServicioLocalStorage ();
+    servicios = obtenerServiciosLocalStorage ();
     servicios.push (servicio);
     localStorage.setItem('servicios', JSON.stringify(servicios));
 
 }
 
 
-function obtenerPlatillosLocalStorage() {
+function obtenerServiciosLocalStorage() {
     let serviciosLocalStorage;
 
     if(localStorage.getItem('servicios') === null) {
-        servicioLocalStorage = [];
+        serviciosLocalStorage = [];
     }else {
-        servicioLocalStorage = JSON.parse(localStorage.getItem('servicios'));
+        serviciosLocalStorage = JSON.parse(localStorage.getItem('servicios'));
     }
-    return servicioLocalStorage;
+    return serviciosLocalStorage;
 }
 
 function leerLocalStorage() {
-    
-   
+    let serviciosLocalStorage;
 
-    servicioLocalStorage = obtenerPlatillosLocalStorage();
+    serviciosLocalStorage = obtenerServiciosLocalStorage();
 
-    servicioLocalStorage.forEach(function(servicio){
-        const nuevoServicio = document.createElement('p');
+    serviciosLocalStorage.forEach(function(servicio){
+        const nuevoServicio = document.createElement('div');
         nuevoServicio.innerHTML = `
             <p> ${servicio.titulo} </p>
             <p> ${servicio.precio} </p>
@@ -81,3 +96,15 @@ function leerLocalStorage() {
         contenedorServiciosCarrito.appendChild(nuevoServicio);
     });
 }
+
+
+let finalizar = document.getElementById("reservar"); 
+
+finalizar.addEventListener('click' , () => {
+    alert(
+        'Has reservado exitosamente'
+      );
+
+      vaciarCarrito();
+})
+
